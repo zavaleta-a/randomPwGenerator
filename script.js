fbdbcf64e2eb3597b871bf589ad75e495f1b2249
Alfredo Zavaleta <azavaleta07@gmail.com>
2021-08-13 16:31:39 -0500

    created var on line 15 to add all of the characters available for the password

--- script.js
+++ script.js
@@ -3,29 +3,36 @@ var generateBtn = document.querySelector("#generate");
 
 // First, we need to create strings to be used in password
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  console.log(upperCase.length);
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  console.log(lowerCase.length);
 var numbers = "0123456789";
+  console.log(numbers.length);
 var spcchar = "!@#$%^&*-_?";
-var passwordLenght = (14);
-  console.log("Maximum number of characters");
+  console.log(spcchar.length);
+var passwordLength = (8);
+  console.log("Maximum number of characters in password is 8");
+var characters = upperCase + lowerCase + numbers + spcchar;
+  console.log("Length of characters available for password", upperCase.length + lowerCase.length + numbers.length + spcchar.length);
+
 
 // Write password to the #password input
 function writePassword() {
-  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     console.log(upperCase);
-  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     console.log(lowerCase);
-  let numbers = "0123456789"
+  var numbers = "0123456789"
     console.log(numbers);
-  let spcchar = "!@#$%^&*-_?";
+  var spcchar = "!@#$%^&*-_?";
     console.log(spcchar);
   var password = "";
     console.log("Empty string");
 
 //Need a loop to randomize and create password
 //Need to make sure it knows when to stop adding characters to the password
-for (var i = 0; i < 10; i++){
-  let writePassword = upperCase[Math.floor(Math.random() * 10)];
+for (var i = 0; i < passwordLength; i++){
+  var generateBtn = upperCase[Math.floor(Math.random() * passwordLength)];
   password += generateBtn;
 }
 
