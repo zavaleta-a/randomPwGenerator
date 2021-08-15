80ce0ce79caabf64ba7d77715a2c786e8e7b4b8a
Alfredo Zavaleta <zavaleta@Alfredos-MacBook-Pro.local>
2021-08-14 21:29:28 -0500

    password given had one more character than the password length limit.  corrected the index in line 34 from 0 to 1

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // First, we need to create strings to be used in password
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   console.log(upperCase.length);
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   console.log(lowerCase.length);
-var numbers = "0123456789";
+
+  var numbers = "0123456789";
   console.log(numbers.length);
-var spcchar = "!@#$%^&*-_?";
+
+  var spcchar = "!@#$%^&*-_?";
   console.log(spcchar.length);
-var passwordLength = (8);
-  console.log("Maximum number of characters in password is 8");
-var characters = upperCase + lowerCase + numbers + spcchar;
+
+  var passwordLength = (9);
+  console.log("Maximum number of characters in password is 9");
+
+  var characters = upperCase + lowerCase + numbers + spcchar;
   console.log("Length of characters available for password", upperCase.length + lowerCase.length + numbers.length + spcchar.length);
 
 
 // Write password to the #password input
 function writePassword() {
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    console.log(upperCase);
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    console.log(lowerCase);
-  var numbers = "0123456789"
-    console.log(numbers);
-  var spcchar = "!@#$%^&*-_?";
-    console.log(spcchar);
+  var characters = upperCase + lowerCase + numbers + spcchar;
+    console.log("Characters from all strings to be used in password");
   var password = "";
     console.log("Empty string");
 
 //Need a loop to randomize and create password
 //Need to make sure it knows when to stop adding characters to the password
-for (var i = 0; i < passwordLength; i++){
-  var generateBtn = upperCase[Math.floor(Math.random() * passwordLength)];
+
+for (var i = 1; i <= passwordLength; i++){
+  var generateBtn = characters[Math.floor(Math.random() * passwordLength)];
   password += generateBtn;
 }
 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; 
 
 }
 
@@ -46,3 +46,6 @@ for (var i = 0; i < passwordLength; i++){
 generateBtn.addEventListener("click", writePassword);
 
 //Clean up code to comply with DRY principle.  Check to see if you really need lines 5 through 12 since the variables are defined inside the function
+
+
+// Add prompts asking user to select password criteria
